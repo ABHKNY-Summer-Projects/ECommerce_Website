5dd900cb83b1d94fdb5d2675b1d91f41e8ed540a
poseidon2022 <kidusm82@gmail.com>
2024-07-23 11:39:56 +0300

    folder structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# Summer Project 01
+
+Welcome to Online Market Place, summer project 01.
+
+
+## Authors
+
+List of [contributors](https://github.com/ABHKNY-Summer-Projects/people) who participated in this project.
+
+## License
+
+This project is licensed under the GNU License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+    console.log(req.body)
+    res.send("Hello World")
+})
+
+const port = process.env.PORT || 8080
+try {
+    app.listen(port, () => {
+        console.log(`App listening on port: ${port}`)
+    })
+}
+catch(error) {
+    console.log(error)
+    process.exit(1)
+}
+
+
