b89b95b3ab83e73fa4feecbb6c81ab1a7ae86bcd
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-07-25 18:16:18 +0300

    Finish Setting Up User Authentication

--- server/index.js
+++ server/index.js
@@ -1,27 +1,39 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-dotenv.config()
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+// Import passport
+const passport = require("passport");
 
+// Define application port
+const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-    console.log(req.body)
-    res.send("Hello World")
-})
+// Import authentication middlewares
+const middlewares = require('./middleware/Authentication_Middlewares')
 
-const port = process.env.PORT || 8080
-try {
-    app.listen(port, () => {
-        console.log(`App listening on port: ${port}`)
-    })
-}
-catch(error) {
-    console.log(error)
-    process.exit(1)
-}
+// Incorporate middlewares
+middlewares.setViewEngine(app);
+middlewares.urlencoded(app);
+middlewares.flash(app);
+middlewares.initializeSession(app);
+middlewares.initializePassport(app, passport);
+const checkAuthenticated = middlewares.checkAuthenticated
+const checkNotAuthenticated = middlewares.checkNotAuthenticated
 
+// Import Authentication Controller
+const Authentication_Controller = require('./controllers/Authentication_Controller');
+
+// REMEMBER TO PUT THE ROUTES IN THEIR RESPECTIVE FOLDERS
+
+app.get('/', Authentication_Controller.render_home);
+
+app.get('/users/login', checkAuthenticated, Authentication_Controller.render_user_login);
+app.get('/users/signup', checkAuthenticated, Authentication_Controller.render_user_signup);
+app.get('/users/dashboard', checkNotAuthenticated, Authentication_Controller.render_user_dashboard);
+app.get('/users/logout', Authentication_Controller.handle_user_logOut);
+app.post('/users/signup', Authentication_Controller.handle_user_signUp);
+app.post('/users/login', Authentication_Controller.handle_user_login);
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
 
