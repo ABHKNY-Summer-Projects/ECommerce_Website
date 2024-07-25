f1c12b3bf368905d090498f9767e968de57ccf80
poseidon2022 <kidusm82@gmail.com>
2024-07-25 18:05:24 +0300

    Remote DB setup

--- server/models/db.js
+++ server/models/db.js
@@ -1,15 +1,32 @@
-const {Client} = require("pg")
+const {Client, Pool} = require("pg")
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
-let client = new Client({
-    user: process.env.DBUSER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: process.env.DBNAME
-})
+let client;
+if (process.env.ENVIRONMENT === "development") {
 
+    client = new Client({
+        user: process.env.DBUSER,
+        password: process.env.PASSWORD,
+        host: process.env.HOST,
+        port: process.env.DBPORT,
+        database: process.env.DBNAME
+    })
+}
+
+else {
+
+    const connectionString = "postgresql://summer01_user:rkfjpGTSX1aLYzQydXDnWBA27wB7xSfN@dpg-cqh3aaaju9rs73eff2u0-a.oregon-postgres.render.com/summer01"
+    client = new Pool({
+        max: 5,
+        min: 2,
+        idleTimeoutMillis: 600000,
+        connectionString: connectionString,
+        ssl : {
+            require: true
+        }
+    })
+}
 
 client.connect()
 .then(()=>{
@@ -17,7 +34,6 @@ client.connect()
 }
 )
 .catch((err) => {
-    console.log(process.env.DBNAME)
     console.error("Error connecting to the database: ", err.stack)
 })
 
