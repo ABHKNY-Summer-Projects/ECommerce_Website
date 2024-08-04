7c2d454b7a20ce4d8c7c1a8ef224fdc4b7375bce
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-04 12:08:15 +0300

    saving

---
+++ client/src/pages/order_management/SaveChanges.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SaveChanges() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Save Changes</h1>
+      <p>Here you can save changes to your orders.</p>
+    </div>
+  );
+}
+
+export default SaveChanges;
