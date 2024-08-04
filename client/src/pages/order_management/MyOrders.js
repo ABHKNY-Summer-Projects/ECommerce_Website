3231495f360e3dca6cfc94cfa54bd284bc98409a
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-04 12:07:13 +0300

    order

---
+++ client/src/pages/order_management/MyOrders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyOrders() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">My Orders</h1>
+      <p>Here you can view all your orders.</p>
+    </div>
+  );
+}
+
+export default MyOrders;
