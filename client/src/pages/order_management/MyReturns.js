c8932f9ccb6d3d3fd69810d8ea366e07e52601a2
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-04 12:07:25 +0300

    ccd

---
+++ client/src/pages/order_management/MyReturns.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyReturns() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">My Returns</h1>
+      <p>Here you can manage your returns.</p>
+    </div>
+  );
+}
+
+export default MyReturns;
